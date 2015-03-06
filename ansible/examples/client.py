fd6483987a9f4fd0fa25eeafc9ee2ea6fe1d10b4
twneale <twneale@gmail.com>
2015-03-05 22:32:51 -0500

    Revert accidental checkin of IP address

--- ansible/examples/client.py
+++ ansible/examples/client.py
@@ -5,7 +5,7 @@ import zmq
 def client():
     context = zmq.Context()
     socket = context.socket(zmq.REQ)
-    socket.connect("tcp://ec2-54-82-68-80.compute-1.amazonaws.com:5559")
+    socket.connect("tcp://localhost:5559")
 
     def send(string):
         obj = dict(annotators='tokenize,ssplit,pos,lemma,parse', text=string)
