0461c16255b7d38fba6b1e6123eae2063a4a6ebb
twneale <twneale@gmail.com>
2015-03-10 11:44:51 -0400

    Finally have supervisor stopping the worker correctly

--- supervisor/bash/run_worker.sh
+++ supervisor/bash/run_worker.sh
@@ -1,21 +1,20 @@
 #! /bin/bash
 
-cidfile=$1
 
 function startup {
   # Run the container.
-  echo "Starting worker ${cidfile}"
-  docker run --link broker:broker --cidfile=$cidfile -w /corenlp/scala corenlp sbt run
+  echo "Starting worker $CORENLP_WORKER_CID_FILE"
+  docker run --link broker:broker --cidfile=$CORENLP_WORKER_CID_FILE -w /corenlp/scala corenlp sbt run
 }
 
 function shutdown {
   echo "Recieved signal; exiting"
   # Kill the docker container.
-  echo "Killing container ${cidfile}"
-  docker kill $(<${cidfile})
+  echo "Killing container $CORENLP_WORKER_CID_FILE"
+  docker kill $(<$CORENLP_WORKER_CID_FILE)
   # Remove the container id file.
   echo "Removing cid file"
-  rm $cidfile 
+  rm $CORENLP_WORKER_CID_FILE
 }
 
 # Properly handle SIGTERM from supervisor.
